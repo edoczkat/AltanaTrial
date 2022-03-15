bdc06db3f58c5893ad177895041298edf95bbaf9
Eric Doczkat <edoczkat46@gmail.com>
2022-03-15 17:32:35 -0400

    change url

---
+++ project/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+// Removed logo since we don't need it anymore
+import './App.css';
+class App extends Component {
+  render() {
+    return (
+      <div classname="container">
+        <h1>Hello World!</h1>
+        <p>I just created my first React App</p>
+      </div>
+    );
+  }
+}
+export default App;
+
+
+// home: https://qualityremovalservices.com/
+// Emergency Service: https://qualityremovalservices.com/emergency-service/
+// Interior Demolition: https://qualityremovalservices.com/interior-demolition/
+// Exterior Demolition: https://qualityremovalservices.com/exterior-demolition/
+// Hauling: https://qualityremovalservices.com/hauling/
+// Gallery: https://qualityremovalservices.com/gallery/
+// Reviews: https://qualityremovalservices.com/reviews/
+// Contact Us: https://qualityremovalservices.com/contact-us/
